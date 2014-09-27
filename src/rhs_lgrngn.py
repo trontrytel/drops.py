be4c66f42c7c927f4dbe99bb8617c59f555acf42
djarecka <djarecka@gmail.com>
2014-09-27 13:19:48 -0600

    adding a simple hdf output

--- src/rhs_lgrngn.py
+++ src/rhs_lgrngn.py
@@ -2,6 +2,7 @@ from libcloudphxx import lgrngn
 from numpy import frombuffer, arange, ndarray
 from io import open
 from os import mkdir
+import h5py
 
 class rhs_lgrngn:
 
@@ -41,12 +42,17 @@ class rhs_lgrngn:
       mkdir(self.outdir)
     except OSError:
       pass
+    try:
+      mkdir(self.outdir + "/hdf_output")
+    except OSError:
+      pass
     self.out_snd = open(self.outdir + "/sounding.txt", mode='w')
     self.out_snd.write(u"#rhod [kg/m3]\tth_d [K] (theta dry!)\tr_v [kg/kg] (mixing ratio)\tM0 [TODO]\tM1 [TODO]\tM2 [TODO]\tM3 [TODO]\tS_VI [kg/kg]\tH [kg/kg]\tSO2 [kg/kg]\n")
     self.out_dry = open(self.outdir + "/spec_dry.txt", mode='w')
     self.out_dry.write(u"#r_d [m] (left bin edge)\tn [kg-1] (per mass of dry air)\n")
     self.out_wet = open(self.outdir + "/spec_wet.txt", mode='w')
     self.out_wet.write(u"#r_w [m] (left bin edge)\tn [kg-1] (per mass of dry air)\n")
+       
 
   def step(self, rhod, th_d, r_v, dot_th, dot_rv):
     th_d_copy = th_d.copy()
@@ -57,6 +63,10 @@ class rhs_lgrngn:
     dot_rv += (r_v_copy - r_v) / self.dt
 
   def diag(self, rhod, th_d, r_v, t):
+    #opening hdf files
+    fhdf_spec = h5py.File(self.outdir + "/hdf_output/spec_drywet_" + str(int(t)) + ".hdf", mode='w')
+    fhdf_sound = h5py.File(self.outdir + "/hdf_output/sounding_" + str(int(t)) + ".hdf", mode='w')
+
     # helper for gnuplot-readable output
     def save(out, xx, yy):
       for x, y in zip(xx, yy):
@@ -70,6 +80,14 @@ class rhs_lgrngn:
       self.prtcls.diag_dry_mom(0)
       bins[i] = frombuffer(self.prtcls.outbuf())
     save(self.out_dry, self.bins_dry[0:-1], bins)
+    # outputting to hdf
+    dry_r_h5 = fhdf_spec.create_dataset("dry_radius", (bins.size,), dtype='f')
+    dry_r_h5.attrs["Units"] = "m"
+    dry_r_h5[...] = self.bins_dry[:-1]
+    dry_n_h5 = fhdf_spec.create_dataset("dry_number", (bins.size,), dtype='f')
+    dry_n_h5[...] = bins
+    dry_n_h5.attrs["Units"] = "1/kg"
+
 
     # outputting spec_wet.txt
     bins = ndarray((self.bins_wet.size - 1,))
@@ -78,17 +96,38 @@ class rhs_lgrngn:
       self.prtcls.diag_wet_mom(0)
       bins[i] = frombuffer(self.prtcls.outbuf())
     save(self.out_wet, self.bins_wet[0:-1], bins)
+    # outputting to hdf
+    wet_r_h5 = fhdf_spec.create_dataset("wet_radius", (bins.size,), dtype='f')
+    wet_r_h5[...] = self.bins_wet[:-1]
+    wet_r_h5.attrs["Units"] = "m"
+    wet_n_h5 = fhdf_spec.create_dataset("wet_number", (bins.size,), dtype='f')
+    wet_n_h5[...] = bins
+    wet_n_h5.attrs["Units"] = "1/kg"
+
+
 
     # outputting sounding.txt
     self.out_snd.write(u"%g" % (rhod))
     self.out_snd.write(u"\t%g" % (th_d))
     self.out_snd.write(u"\t%g" % (r_v))
+    # outputting to hdf
+    rho_h5 = fhdf_sound.create_dataset("rhod", (rhod.size,), dtype='f')
+    rho_h5[...] = rhod
+    rho_h5.attrs["Units"] = "kg/m3"
+    thd_h5 = fhdf_sound.create_dataset("thd", (th_d.size,), dtype='f')
+    thd_h5[...] = th_d
+    thd_h5.attrs["Units"] = "K"
+    rv_h5 = fhdf_sound.create_dataset("rv", (r_v.size,), dtype='f')
+    rv_h5[...] = r_v
+    rv_h5.attrs["Units"] = "kg/kg"
+
 
     ## cloud water
     self.prtcls.diag_wet_rng(.5e-6, 25e-6) 
     for k in range(0,4):
       self.prtcls.diag_wet_mom(k)
       self.out_snd.write(u"\t%g" % (frombuffer(self.prtcls.outbuf())))
+
 
     ## chem stuff
     self.prtcls.diag_wet_rng(0,1) # 0 ... 1 m #TODO: consider a select-all option?
