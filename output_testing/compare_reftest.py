8580c4b8d5e96c2dab8f91904abc9881194a6937
djarecka <djarecka@gmail.com>
2014-10-02 15:40:04 -0600

    adding printing of dims. of hdf variables

--- output_testing/compare_reftest.py
+++ output_testing/compare_reftest.py
@@ -10,10 +10,17 @@ def comparing_hdf(dir_new, dir_ref, filetype='hdf'):
             f_ref = h5py.File(dir_ref+"/hdf_output/" + filename, "r")
             f_new = h5py.File(dir_new+"/hdf_output/" + filename, "r")
             for key in f_ref.keys():
-                 if (np.array(f_ref[key]) == np.array(f_new[key])).all():
-                     print key + " ok "
-                 else:
-                     print "Problems!! " +key + " differ in " + filename 
+                if (np.array(f_ref[key]) == np.array(f_new[key])).all():
+                    print "\n", "values of " +  key + " OK "
+                    if key not in ["time", "bins_dry", "bins_wet"]:
+                        print "new files: dimensions of ", key, ":"
+                        for dim in f_new[key].dims:
+                            print "name, unit, values", dim[0].name, dim.label, dim[0][:]
+                    
+                else:
+                     print "Problems!! values of " + key + " differ"
+            
+                
 
 
 def main(dir_new, dir_ref):
