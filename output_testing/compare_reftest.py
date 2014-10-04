8ea00707a777f52ccc5159d61a8b89886f134414
djarecka <djarecka@gmail.com>
2014-10-03 18:13:18 -0600

    printing units

--- output_testing/compare_reftest.py
+++ output_testing/compare_reftest.py
@@ -13,6 +13,7 @@ def comparing_hdf(dir_new, dir_ref, filetype='hdf'):
                 if (np.array(f_ref[key]) == np.array(f_new[key])).all():
                     print "\n", "values of " +  key + " OK "
                     if key not in ["time", "bins_dry", "bins_wet", "mom_ord", "chem_sp"]:
+                        print "new files: unit of ", key, "-", f_new[key].attrs["Units"]
                         print "new files: dimensions of ", key, ":"
                         for dim in f_new[key].dims:
                             print "name, unit, values", dim[0].name, dim.label, dim[0][:]
