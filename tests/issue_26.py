19e0251b14a067c123003d65adf1ec6a57ae0f57
Sylwester Arabas <sarabas@igf.fuw.edu.pl>
2014-11-04 11:25:34 +0100

    Fixing issue 26 with negative S_max, adding a relevant test case. Closes #26.

---
+++ tests/issue_26.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import tempfile, os, sys, string
+
+outdir = tempfile.mkdtemp()
+srcdir = sys.argv[1]
+
+os.system(srcdir + "/drops.py --outdir " + outdir + 
+  " --T 288 --p 100000 --RH 0.999 --w 0.62593 --outfreq 10000" + 
+  " --dt 0.5 --nt 320 lgrngn --sd_conc 128 --kappa 0.6052" + 
+  " --n_tot 930759410 317786280 --meanr 1.1429e-07 4.3822e-08" + 
+  " --gstdv 1.4745 1.4466"
+)
+
+f = open(outdir + "/stats.gpi")
+for line in f:
+  flds = string.split(line, ' = ')
+  if flds[0] == 'S_max_RH':
+    print flds[1]
+    if float(flds[1]) < 1: sys.exit(1)
+    else: sys.exit(0)
+sys.exit(1)
